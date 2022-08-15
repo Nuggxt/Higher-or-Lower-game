411d7772a1e500cfa9cd534c691ddde4d908a752
NuggxtDev <devnuggetcodes@gmail.com>
2022-08-15 08:18:29 -0600

    reverted Javascript and fixed dropshadow on button

--- index.js
+++ index.js
@@ -7,11 +7,3 @@ if (random > 50) {
   console.log("Your number is higher than 50");
   console.log(random);
 }
-
-const button = document.querySelector('button');
-
-function greet (); {
-  const name = prompt('Hello there! What is your name?')
-  alert(`Hello ${name}, Ready to play?`);
-}
- button.addEventListener ('click', greet);
