1616861bb87c3a47fea9af0cc3785b784c9f5a28
NuggxtDev <devnuggetcodes@gmail.com>
2022-08-15 08:42:05 -0600

    minor UI updates and aded how to play: section.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let random = Math.floor(Math.random() * 100) + 1;
 if (random < 50) {
-  console.log("You number is lower than 50");
+  console.log("You number is lower than 50. You lose...");
   console.log(random);
 }
 if (random > 50) {
-  console.log("Your number is higher than 50");
+  console.log("Your number is higher than 50. You win!");
   console.log(random);
 }
