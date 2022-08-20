ee118a8aba6986fbd22d324668ecbee3474076fe
NuggxtDev <devnuggetcodes@gmail.com>
2022-08-20 14:39:46 -0600

    Testing some things for now

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Gets random number
 let random = Math.floor(Math.random() * 100) + 1;
 if (random < 50) {
   console.log("Your number is lower than 50. You lose...");
@@ -7,6 +8,8 @@ if (random > 50) {
   console.log("Your number is higher than 50. You win!");
   console.log(random);
 }
+
+// Changes the color of the button text on click.
 const btn = document.getElementById('btn');
 
 btn.addEventListener('click', function onClick(event){
