be18809ca879b8c5f0e77d4306afe5870b1d750d
NuggxtDev <devnuggetcodes@gmail.com>
2022-08-27 11:03:23 -0500

    Small Update
    Update to the UI to fit better on mobile. and reworked the JS code a bit to clean it up.

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ let random = Math.floor(Math.random() * 100) + 1;
 if (random < 50) {
   console.log("Your number is lower than 50. You lose...");
   console.log(random);
-}
-if (random > 50) {
+} else if (random > 50) {
   console.log("Your number is higher than 50. You win!");
   console.log(random);
 }
 
 // Changes the color of the button text on click.
-const btn = document.getElementById('btn');
+// const btn = document.getElementById('btn');
 
-btn.addEventListener('click', function onClick(event){
-  event.target.style.color='gold';
+// btn.addEventListener('click', function onClick(event){
+//  event.target.style.color='gold';
 })
