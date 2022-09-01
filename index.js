adbe8bdcfccd72cd24de34263f6a88afa2800e81
NuggxtDev <devnuggetcodes@gmail.com>
2022-09-01 13:10:14 -0600

    Finally git the numbers to display on button click

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // Gets random number
 
-let random = Math.floor(Math.random() * 100) + 1;
-if (random < 50) {
-  console.log(random + " You lose...");
-} else if (random > 50) {
-  console.log(random + " You WIN!!!");
-}
+// let random = Math.floor(Math.random() * 100) + 1;
+// if (random < 50) {
+//  console.log(random + " You lose...");
+// } else if (random > 50) {
+//  console.log(random + " You WIN!!!");
+// }
 
-// Testing some stuff here. Might Delete..
-let button = document.querySelector(".button");
-let div = document.querySelector('.display');
-let count = 0;
+let x;
 
+document.getElementById("rollButton").onclick = function() {
+  x = Math.floor(Math.random() * 100) + 1;
+
+  document.getElementById("xlabel").innerHTML = x;
+};
