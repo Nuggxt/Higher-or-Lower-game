c5892762a0af707f94d36a9f1fe368c05358fd33
NuggxtDev <devnuggetcodes@gmail.com>
2022-09-02 00:32:48 -0600

    Added comments to help better understand whats happening

--- index.js
+++ index.js
@@ -7,10 +7,9 @@
 //  console.log(random + " You WIN!!!");
 // }
 
-let x;
+let x; // Sets the variable to be set up for the random number generation.
 
-document.getElementById("rollButton").onclick = function() {
-  x = Math.floor(Math.random() * 100) + 1;
-
-  document.getElementById("xlabel").innerHTML = x;
+document.getElementById("rollButton").onclick = function() { // Calls the button onclick function
+  x = Math.floor(Math.random() * 100) + 1; // Gets the random number
+  document.getElementById("xlabel").innerHTML = x; // Displays the random number after button click
 };
