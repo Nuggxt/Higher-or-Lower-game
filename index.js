10b5879cf9e34b14a4dc67ec298504c624da4eae
NuggxtDev <devnuggetcodes@gmail.com>
2022-09-02 10:38:24 -0600

    Testing vh in css and added alert for win or lose in index.js, will remove later

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// Gets random number
-
-// let random = Math.floor(Math.random() * 100) + 1;
-// if (random < 50) {
-//  console.log(random + " You lose...");
-// } else if (random > 50) {
-//  console.log(random + " You WIN!!!");
-// }
-
 let x; // Sets the variable to be set up for the random number generation.
 
 document.getElementById("rollButton").onclick = function() { // Calls the button onclick function
   x = Math.floor(Math.random() * 100) + 1; // Gets the random number
   document.getElementById("xlabel").innerHTML = x; // Displays the random number after button click
+  if (x < 50) {
+    alert("You Lose...");
+  } else if (x > 50) {
+    alert("You Win!!!");
+  }
 };
