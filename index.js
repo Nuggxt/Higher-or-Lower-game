2cc1a2c62e850a76bf2b7c4166da0ec5267fc5d3
NuggxtDev <devnuggetcodes@gmail.com>
2022-09-02 11:15:49 -0600

    Added ylabel to display to display Win or Lose, But havent started on it yet. Fixed minor css padding and Strated on Y in js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 let x; // Sets the variable to be set up for the random number generation.
+let y;
 
-document.getElementById("rollButton").onclick = function() { // Calls the button onclick function
+document.getElementById("rollButton").onclick = function () {
+  // Calls the button onclick function
   x = Math.floor(Math.random() * 100) + 1; // Gets the random number
   document.getElementById("xlabel").innerHTML = x; // Displays the random number after button click
-  if (x < 50) {
-    alert("You Lose...");
-  } else if (x > 50) {
-    alert("You Win!!!");
-  }
 };
+
