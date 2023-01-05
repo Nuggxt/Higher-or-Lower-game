01e35a8cb1ed4eda7953924c0be1ab4cdaa03737
NuggxtDev <devnuggetcodes@gmail.com>
2023-01-05 16:37:53 -0600

    Small JS code update
    Small update to the JS code. Took out the 2 variables and replaced the console log.

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 let x; // Sets the variable to be set up for the random number generation.
 let y;
-const win = 'You Win!';
-const lose = 'You Lose...';
 
 document.getElementById("rollButton").onclick = function () {
   // Calls the button onclick function
   x = Math.floor(Math.random() * 100) + 1; // Gets the random number
   document.getElementById("xlabel").innerHTML = x; // Displays the random number after button click
-  if(x > 50){
-    console.log(win)
-  }else {
-    console.log(lose)
+  if (x > 50) {
+    console.log('You Win!')
+  } else {
+    console.log('You Lose...')
   }
 };
