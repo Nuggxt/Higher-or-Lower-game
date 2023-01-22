4e8c89a024f03917c146f6f08efe91bfc9e2ce2c
NuggxtDev <devnuggetcodes@gmail.com>
2023-01-22 00:22:29 -0600

    minor update
    update to the UI. added the border color on  win/lose.
    
    win = limegreen
    lose = red

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ document.getElementById("rollButton").onclick = function () {
   x = Math.floor(Math.random() * 100) + 1; // Gets the random number
   document.getElementById("xlabel").innerHTML = x; // Displays the random number after button click
   if (x > 50) {
-    document.getElementById('xlabel').innerHTML = x + ': You Win! :)';
+    document.getElementById('xlabel').innerHTML = x + ', You Win! :)';
+    document.getElementById('display').style.border = 'solid limegreen';
   } else {
-    document.getElementById('xlabel').innerHTML = x + ': You Lose :(';
+    document.getElementById('xlabel').innerHTML = x + ', You Lose.. :(';
+    document.getElementById('display').style.border = 'solid red';
   }
 };
